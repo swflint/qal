7b246fd843a5faf532576558c517cc03f7566445
Samuel W. Flint <swflint@flintfam.org>
2021-06-03 15:02:57 -0500

    Add in a stub package

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(name = "scrape_acad_library",
+      version = "0.0.1",
+      description = "A tool for systematically querying various academic publishing databases.",
+      author = "Samuel W. Flint",
+      author_email = "swflint@flintfam.org",
+      license = "MIT",
+      packages = find_packages(where = "src"),
+      install_requires = [
+          "requests",
+          "backoff",
+          "urllib3"
+      ])
