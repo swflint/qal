15d252e191575d9d97a7821e4dcb516105dc7e5a
Samuel W. Flint <swflint@flintfam.org>
2021-06-03 15:04:04 -0500

    Add Nix packaging mechanics

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "A tool for automatically searching various academic libraries.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      rec {
+        packages = rec {
+          scrape-acad-library = pkgs.python3.pkgs.buildPythonPackage {
+            pname = "scrape_acad_library";
+            version = "0.0.1";
+            src = ./.;
+            propagatedBuildInputs = with pkgs.python3.pkgs ; [
+              urllib3
+              requests
+              backoff
+            ];
+          };
+
+          pythonEnvironment = pkgs.python3.withPackages (ps: with ps; [
+            urllib3
+            requests
+            backoff
+            ipython
+          ]);
+
+          developmentEnvironment = pkgs.mkShell {
+            buildInputs = [
+              packages.pythonEnvironment
+              pkgs.pre-commit
+            ];
+          };
+        };
+        defaultPackage = packages.scrape-acad-library;
+        devShell = packages.developmentEnvironment;
+      }
+    );
+}
