cbc977b594227233b3c6b0e62c61759e49abc9b4
Samuel W. Flint <swflint@flintfam.org>
2021-06-03 18:41:04 -0500

    Switch Springer Nature to use the new system

--- src/scrape_acad_library/__init__.py
+++ src/scrape_acad_library/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# from .springer import SpringerNature
+from .springer import SpringerNature
 # from .ieeexplore import IEEEXplore
 from .science_direct import ScienceDirect
--- src/scrape_acad_library/springer.py
+++ src/scrape_acad_library/springer.py
@@ -1,70 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import urllib.request
-from urllib.error import HTTPError
-from urllib.parse import urlencode, urlparse
+from .digital_library import DigitalLibrary
 
-import requests
-import backoff
+class SpringerNature(DigitalLibrary):
+    def __init__(self, api_key, max_results = 50, start_result = 1):
+        super().__init__(name = 'springer_nature',
+                         request_type = 'GET',
+                         api_key_name = 'api_key',
+                         api_key = api_key,
+                         query_url = "http://api.springernature.com/meta/v2/json",
+                         start_key = 's',
+                         num_results_key = 'p',
+                         default_num_results = max_results,
+                         default_start = start_result,
+                         query_option_information = { 'query_text': 'q' })
 
-import json
-import simplejson
-
-class SpringerNature:
-
-    def __init__(self, api_key, max_results = 100, start_result = 1):
-        self.api_key = api_key
-        self.query = ""
-        self.max_results = max_results
-        self.start_result = start_result
-        self.total_results = -1
-        self.error = False
-        self.o_max_results = max_results
-        self.o_start_result = start_result
-        self.request_number = 0
-        self.batch = 0
-
-    def set_query(self, query):
-        self.query = query
-        self.max_results = self.o_max_results
-        self.start_result = self.o_start_result
-        self.total_results = -1
-        self.error = False
-        self.request_number = 0
-        self.batch = 0
-
-    @backoff.on_exception(backoff.expo,
-                          (requests.exceptions.RequestException,
-                           simplejson.errors.JSONDecodeError),
-                          max_tries = 10)
-    def make_request(self):
-        self.request_number += 1
-        print(f"Batch {self.batch}, Request {self.request_number}")
-        response = requests.get(url = "http://api.springernature.com/meta/v2/json",
-                                params = { 'api_key': self.api_key,
-                                           'q': self.query,
-                                           'p': self.max_results,
-                                           's': self.start_result })
-        return response.json()
-        
-    def run_query(self):
-        self.request_number = 0
-        self.batch += 1
-        try:
-            data = self.make_request()
-            self.request_number = 0
-            self.total_results = int(data['result'][0]['total'])
-            self.start_result += int(data['result'][0]['recordsDisplayed'])
-            return data['records']
-        except simplejson.errors.JSONDecodeError:
-            self.error = True
-            return []
-
-    def has_results(self):
-        if self.error:
-            return False
-        elif self.total_results == -1:
-            return True
-        else:
-            return self.start_result < self.total_results
+    def process_results(self, data):
+        self.results_total = int(data['result'][0]['total'])
+        self.start += int(data['result'][0]['recordsDisplayed'])
+        return data['records']
