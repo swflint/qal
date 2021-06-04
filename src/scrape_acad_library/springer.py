08a762e3254cde4011fde323a13de19465d62bfc
Samuel W. Flint <swflint@flintfam.org>
2021-06-04 09:44:25 -0500

    Update conference information selection, include search terms in Springer

--- src/scrape_acad_library/springer.py
+++ src/scrape_acad_library/springer.py
@@ -4,6 +4,24 @@
 from .digital_library import DigitalLibrary
 from .types import Article, Conference
 
+def sanitize_venue(string):
+    string = re.sub(r'proceedings of the', '', string, flags = re.IGNORECASE)
+    string = re.sub(r"[0-9]{1,2}(nd|th|rd|st)", "", string)
+    string = re.sub(r"(first|second|third|fourth|fifth|sixth|seventh|eighth|ninth|tenth|eleventh|twelfth|thirteenth|fourteenth|fifteenth|sixteenth|seventeeth|eighteenth|ninteenth|twentieth|twenty|thirtieth|thirty|fourthieth|fourty|fiftieth|fifty|sixtieth|sixty)-?", "", string, flags = re.IGNORECASE)
+    string = re.sub(r'\u2014', '', string)
+    string = re.sub(r'\u2013', '', string)
+    string = re.sub(r'\'\d{2}', '', string)
+    string = re.sub(r'\u2019[0-9]+', '', string)
+    string = re.sub(r'[0-9]{4}', '', string)
+    string = re.sub(r'Part (v|iv|iii|ii|i)', '', string, flags = re.IGNORECASE)
+    string = re.sub(r'proceedings of', '', string, flags = re.IGNORECASE)
+    string = re.sub(r'volume \d+', '', string, flags=re.IGNORECASE)
+    string = re.sub(r'\s[,:]\s', '', string)
+    string = re.sub(r'\s+\)', '\)', string)
+    string = re.sub(r' - ', ' ', string)
+    string = re.sub(r'\s+', ' ', string).strip()
+    return string
+
 class SpringerNature(DigitalLibrary):
     def __init__(self, api_key, max_results = 50, start_result = 1):
         super().__init__(name = 'springer_nature',
@@ -30,22 +48,26 @@ class SpringerNature(DigitalLibrary):
                 authors.append(author['creator'])
             year = result['publicationDate'][:4]
             if result_type == 'Article':
-                results.append(Article(identifier,
-                                       title,
-                                       authors,
-                                       year,
-                                       journal = result['publicationName'],
-                                       volume = result['volume'],
-                                       issue = result['number'],
-                                       abstract = result.get('abstract'),
-                                       pages = None))
+                result_item = Article(identifier,
+                                      title,
+                                      authors,
+                                      year,
+                                      journal = result['publicationName'],
+                                      volume = result['volume'],
+                                      issue = result['number'],
+                                      abstract = result.get('abstract'),
+                                      pages = None)
+                result_item.add_search_terms('springer_link', self.query_data)
+                results.append()
             elif result_type == 'Chapter ConferencePaper':
-                results.append(Conference(identifier,
-                                          title,
-                                          authors,
-                                          year,
-                                          book_title = result['publicationName'],
-                                          conference = result['conferenceInfo'][0]['confSeriesName'],
-                                          abstract = result.get('abstract'),
-                                          pages = None))
+                result_item = Conference(identifier,
+                                         title,
+                                         authors,
+                                         year,
+                                         book_title = result['publicationName'],
+                                         conference = sanitize_venue(result['publicationName']),
+                                         abstract = result.get('abstract'),
+                                         pages = None)
+                result_item.add_search_terms('springer_link', self.query_data)
+                results.append(result_item)
         return data['records']
