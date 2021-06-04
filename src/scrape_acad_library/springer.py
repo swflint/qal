3300c96e7c5c7f1f426b19b62d52ddc6e434182b
Samuel W. Flint <swflint@flintfam.org>
2021-06-03 20:05:58 -0500

    Create conference/article objects for SpringerNature

--- src/scrape_acad_library/springer.py
+++ src/scrape_acad_library/springer.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 from .digital_library import DigitalLibrary
+from .types import Article, Conference
 
 class SpringerNature(DigitalLibrary):
     def __init__(self, api_key, max_results = 50, start_result = 1):
@@ -19,4 +20,32 @@ class SpringerNature(DigitalLibrary):
     def process_results(self, data):
         self.results_total = int(data['result'][0]['total'])
         self.start += int(data['result'][0]['recordsDisplayed'])
+        results = []
+        for result in data['records']:
+            result_type = result['contentType']
+            identifier = result['doi']
+            title = result['title']
+            authors = []
+            for author in result['creators']:
+                authors.append(author['creator'])
+            year = result['publicationDate'][:4]
+            if result_type == 'Article':
+                results.append(Article(identifier,
+                                       title,
+                                       authors,
+                                       year,
+                                       journal = result['publicationName'],
+                                       volume = result['volume'],
+                                       issue = result['number'],
+                                       abstract = result.get('abstract'),
+                                       pages = None))
+            elif result_type == 'Chapter ConferencePaper':
+                results.append(Conference(identifier,
+                                          title,
+                                          authors,
+                                          year,
+                                          book_title = result['publicationName'],
+                                          conference = result['conferenceInfo'][0]['confSeriesName'],
+                                          abstract = result.get('abstract'),
+                                          pages = None))
         return data['records']
