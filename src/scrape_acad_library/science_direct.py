4ce98d9b2d1652931614621a767abdd99fc3c5c6
Samuel W. Flint <swflint@flintfam.org>
2021-06-07 10:33:05 -0500

    Switch up ScienceDirect API target

--- src/scrape_acad_library/science_direct.py
+++ src/scrape_acad_library/science_direct.py
@@ -6,37 +6,34 @@ from .types import Article
 
 class ScienceDirect(DigitalLibrary):
 
-    def __init__(self, api_key, max_results = 50, start_result = 1):
+    def __init__(self, api_key, max_results = 25, start_result = 1):
         super().__init__(name = "science_direct",
                          request_type = "GET",
                          api_key_name = 'apiKey',
                          api_key = api_key,
-                         query_url = "https://api.elsevier.com/content/metadata/article",
+                         query_url = "https://api.elsevier.com/content/search/sciencedirect",
                          start_key = 'start',
                          num_results_key = 'count',
                          default_num_results = max_results,
                          default_start = start_result,
                          query_option_information = { 'query_text': 'query' },
-                         non_query_parameters = { 'httpAccept': 'application/json',
-                                                  'view': 'COMPLETE' })
+                         non_query_parameters = { 'httpAccept': 'application/json' })
 
     def process_results(self, data):
-        self.results_total = int(data['search-results']['opensearch:totalResults'])
-        self.start += len(data['entry'])
+        self.results_total = int(data['search-results']['openSearch:totalResults'])
+        self.start += len(data['search-results']['results'])
         results = []
-        for result in data['entry']:
-            identifier = result['dc:identifier']
+        for result in data['search-results']['results']:
+            identifier = result['prism:doi']
             title = result['dc:title']
             authors = []
             for author in result['authors']:
-                authors.append(f"{author['given-name']} {author['surname']}")
-            result_type = result['prism:aggregationType']
-            year = result['prism:coverDate'][0]['$'][:4]
-            if result_type == 'journal':
-                result_item = Article(identifier, title, authors, year,
-                                      journal = result['prism:publicationName'],
-                                      volume = result['prism:volume'],
-                                      issue = result['prism:issueIdentifier'],
-                                      abstract = result['prism:teaser'],
-                                      pages = None)
+                authors.append(f"{author['name']}")
+            year = result['prism:coverDate'][:4]
+            result_item = Article(identifier, title, authors, year,
+                                  journal = result['prism:publicationName'],
+                                  volume = None,
+                                  issue = None,
+                                  abstract = None,
+                                  pages = None)
         return results
